c480868930b1b4175db5750e230d74aee7e2efd6
Juan Pablo Gelmi <jpgelmi@gmail.com>
2021-02-11 20:12:06 -0300

    commit

--- script.js
+++ script.js
@@ -13,13 +13,12 @@ $(document).ready(function(){
         }else{
             $('.scroll-up-btn').removeClass("show");
         }
-    });
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
+        $('.navbar .menu li a').click(function(){
+            // applying again smooth scroll on menu items click
+            $('html').css("scrollBehavior", "smooth");
+        });
     });
-
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
