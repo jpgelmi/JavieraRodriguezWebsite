4d59bf353b7b24694c31be29759c93819deee9d9
Juan Pablo Gelmi <jpgelmi@gmail.com>
2021-02-11 19:40:40 -0300

    commit

--- script.js
+++ script.js
@@ -15,13 +15,6 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
     $('.navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
@@ -31,21 +24,6 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Limpia", "Segura", "Integral"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Lorem", "Ipsum", "Dole"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
     });
 
     // owl carousel script
